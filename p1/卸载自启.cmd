84a403bf26497b69270af1580ef80eac6c7919e7
OyaYYDS <OyaYYDS@users.noreply.github.com>
2026-09-05 07:05:21 +0800

    自启安装/卸载统一走设置窗口，移除冗余 bat；文档同步

--- p1/卸载自启.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo Removing autostart for current user...
-AsterAudioRouter.exe --uninstall
-pause
--- p1/安装自启.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo Installing autostart for current user...
-AsterAudioRouter.exe --install
-pause
